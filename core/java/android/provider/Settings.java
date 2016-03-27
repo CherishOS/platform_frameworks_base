8ab50a23fae2db79dbb5f397a75054a6909c3293
Lars Greiss <kufikugel@googlemail.com>
2016-03-26 21:18:14 -0400

    HeadsUp: add timeout option (1/2)
    Forward ported to marshmallow/nougat By: BeansTown106
    this was basically a rewrite as everything about it changed
    
    Squashed:
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Fri, 15 Apr 2022 21:20:04 +0530
        Subject: HeadsUp: Change heads up timeout to seconds
    
    Change-Id: I9c88d67d25440dff8f545e1330da672be5d36913
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7688,6 +7688,12 @@ public final class Settings {
         public static final String DEFAULT_MAX_ALARM_VOLUME = "default_max_alarm_volume";
 
         /**
+         * Heads up timeout configuration
+         * @hide
+         */
+        public static final String HEADS_UP_TIMEOUT = "heads_up_timeout";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
