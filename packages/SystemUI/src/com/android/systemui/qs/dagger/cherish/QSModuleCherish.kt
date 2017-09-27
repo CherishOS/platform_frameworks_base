1f46405be74ea640b8c5a95fa5abc207b29f2097
Roman Birg <roman@cyngn.com>
2017-09-27 21:01:50 +0200

    SystemUI: Add Compass tile
    neobuddy89:
    * Set default state to inactive
    * Remove slashed icon
    * Add check if device supports compass
    * Disable long click for Compass tile
    * Code clean up and adapt to Android 13, 14
    * Combine compass check in tile itself
    
    Squashed:
    
        From: timjosten <timjosten@gmail.com>
        Date: Thu, 18 Aug 2022 18:10:47 +0000
        Subject: SystemUI: Fix QS compass tile spam & instability
    
        E ViewRootImpl: Accessibility content change on non-UI thread. Future Android versions will throw an exception.
        E ViewRootImpl: android.view.ViewRootImpl$CalledFromWrongThreadException: Only the original thread that created a view hierarchy can touch its views.
        E ViewRootImpl:  at android.view.ViewRootImpl$SendWindowContentChangedAccessibilityEvent.runOrPost(ViewRootImpl.java:10375)
        E ViewRootImpl:  at android.view.ViewRootImpl.postSendWindowContentChangedCallback(ViewRootImpl.java:9203)
        E ViewRootImpl:  at android.view.ViewRootImpl.notifySubtreeAccessibilityStateChanged(ViewRootImpl.java:9383)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.View.notifySubtreeAccessibilityStateChangedIfNeeded(View.java:14002)
        E ViewRootImpl:  at android.view.View.setRotation(View.java:17015)
    
    shuttercat: add in UTILITES group
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/dagger/cherish/QSModuleCherish.kt
+++ packages/SystemUI/src/com/android/systemui/qs/dagger/cherish/QSModuleCherish.kt
@@ -34,6 +34,7 @@ import com.android.systemui.qs.tiles.NfcTile
 import com.android.systemui.qs.tiles.ReadingModeTile
 import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.PowerShareTile
+import com.android.systemui.qs.tiles.CompassTile
 import com.android.systemui.qs.tiles.UsbTetherTile
 import com.android.systemui.qs.tiles.VpnTile
 import com.android.systemui.qs.tiles.base.shared.model.QSTileConfig;
@@ -78,6 +79,12 @@ interface QSModuleCherish {
     @IntoMap
     @StringKey(DataSwitchTile.TILE_SPEC)
     fun bindDataSwitchTile(dataSwitchTile: DataSwitchTile): QSTileImpl<*>
+
+    /** Inject CompassTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(CompassTile.TILE_SPEC)
+    fun bindCompassTile(compassTile: CompassTile): QSTileImpl<*>
 
     /** Inject HeadsUpTile into tileMap in QSModule */
     @Binds
@@ -263,6 +270,21 @@ interface QSModuleCherish {
                 ),
                 instanceId = uiEventLogger.getNewInstanceId(),
                 category = TileCategory.DISPLAY
+            )
+        }
+
+       @Provides
+        @IntoMap
+        @StringKey(CompassTile.TILE_SPEC)
+        fun provideCompassConfig(uiEventLogger: QsEventLogger): QSTileConfig {
+            return QSTileConfig(
+                tileSpec = TileSpec.create(CompassTile.TILE_SPEC),
+                uiConfig = QSTileUIConfig.Resource(
+                    iconRes = R.drawable.ic_qs_compass,
+                    labelRes = R.string.quick_settings_compass_label
+                ),
+                instanceId = uiEventLogger.getNewInstanceId(),
+                category = TileCategory.UTILITIES
             )
         }
 
