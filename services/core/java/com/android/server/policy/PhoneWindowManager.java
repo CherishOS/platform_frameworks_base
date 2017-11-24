2b13e4c8835b41e568fb2e9f6d4a275c668507b1
Simon Shields <simon@lineageos.org>
2017-11-24 16:05:19 +1100

    PhoneWindowManager: add VolumeKeyHandler
    We can't use a generic DeviceKeyHandler here, because we don't want to
    override some of the AOSP logic (silencing incoming calls, screenshots)
    and we need to know if the device is interactive or not (for screen-off
    music controls)
    
    Change-Id: I485c2f6006c5bbd358ba0cbd32917689826c5c6d
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -272,6 +272,7 @@ import com.android.server.wm.WindowManagerInternal.AppTransitionListener;
 import dalvik.system.PathClassLoader;
 import com.android.internal.util.cherish.DeviceKeysConstants.Action;
 import com.android.internal.util.cherish.ActionUtils;
+import com.android.internal.util.cherish.VolumeKeyHandler;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -791,6 +792,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private final List<DeviceKeyHandler> mDeviceKeyHandlers = new ArrayList<>();
 
     private final boolean mVisibleBackgroundUsersEnabled = isVisibleBackgroundUsersEnabled();
+    private VolumeKeyHandler mVolumeKeyHandler;
 
     private static final int MSG_DISPATCH_MEDIA_KEY_WITH_WAKE_LOCK = 3;
     private static final int MSG_DISPATCH_MEDIA_KEY_REPEAT_WITH_WAKE_LOCK = 4;
@@ -5801,6 +5803,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     // {@link interceptKeyBeforeDispatching()}.
                     result |= ACTION_PASS_TO_USER;
                } else if ((result & ACTION_PASS_TO_USER) == 0 && !mWakeOnVolumeKeyPress) {
+                    if (mVolumeKeyHandler.handleVolumeKey(event, interactive)) {
+                        break;
+                    }
                     // If we aren't passing to the user and no one else
                     // handled it send it to the session manager to
                     // figure out.
@@ -7121,6 +7126,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 mKeyguardDelegate.onBootCompleted();
             }
         }
+        mVolumeKeyHandler = new VolumeKeyHandler(mContext);
         mSideFpsEventHandler.onFingerprintSensorReady();
         startedWakingUp(Display.DEFAULT_DISPLAY_GROUP, PowerManager.WAKE_REASON_UNKNOWN);
         finishedWakingUp(Display.DEFAULT_DISPLAY_GROUP, PowerManager.WAKE_REASON_UNKNOWN);
