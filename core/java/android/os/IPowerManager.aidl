ffb48e643fa0f9d8153d523344a23be891ff3424
Bruno Martins <bgcngm@gmail.com>
2017-12-01 23:40:24 +0000

    PowerManager: Add proximity check on wake
    Co-authored-by: Adnan Begovic <adnan@cyngn.com>
    Co-authored-by: Christopher R. Palmer <crpalmer@gmail.com>
    Co-authored-by: Danesh M <daneshm90@gmail.com>
    Co-authored-by: dankoman <dankoman30@gmail.com>
    Co-authored-by: Erica Chang <echang@cyngn.com>
    Co-authored-by: Han Wang <416810799@qq.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Co-authored-by: Pat Erley <perley@cyngn.com>
    Co-authored-by: Roman Birg <roman@cyngn.com>
    Co-authored-by: Schubi <schubi@erlangen.ccc.de>
    Co-authored-by: Scott Mertz <scott@cyngn.com>
    Co-authored-by: Steve Kondik <steve@cyngn.com>
    Co-authored-by: vm03 <vasy@vasy.ru>
    Change-Id: Ia5ff446d88435773acccc7467e66009000dc20cb
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/os/IPowerManager.aidl
+++ core/java/android/os/IPowerManager.aidl
@@ -180,4 +180,6 @@ interface IPowerManager
 
     // Lineage custom API
     void rebootCustom(boolean confirm, String reason, boolean wait);
+
+    void wakeUpWithProximityCheck(long time, int reason, String details, String opPackageName, int displayId);
 }
--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7092,6 +7092,12 @@ public final class Settings {
         public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
 
         /**
+         * Whether Proximity on Wake is Enabled or not
+         * @hide
+         */
+        public static final String PROXIMITY_ON_WAKE = "proximity_on_wake";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
