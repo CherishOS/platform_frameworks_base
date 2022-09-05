000450385861ef381ee3c6413028688ecfc6b931
cjybyjk <cjybyjk@zjnu.edu.cn>
2022-09-05 04:14:52 +0000

    base: support per-app volume [2/3]
    [neobuddy89]
    Adapted for A14 QPR3
    
    Change-Id: I10d3b0ec2cba6778d6225269b238863137efb21b
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7254,6 +7254,12 @@ public final class Settings {
         public static final String PROXIMITY_ON_WAKE = "proximity_on_wake";
 
         /**
+         * Show app volume rows in volume panel
+         * @hide
+         */
+        public static final String SHOW_APP_VOLUME = "show_app_volume";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -22652,6 +22658,12 @@ public final class Settings {
         @SdkConstant(SdkConstant.SdkConstantType.ACTIVITY_INTENT_ACTION)
         public static final String ACTION_VOLUME =
                 "android.settings.panel.action.VOLUME";
+
+        /**
+         * @hide
+         */
+        public static final String ACTION_APP_VOLUME =
+                "android.settings.panel.action.APP_VOLUME";
     }
 
     /**
