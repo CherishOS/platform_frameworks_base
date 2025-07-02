f7cee8cdeeeaa6d144dad9dbe678b470060f7d15
zakaryan2004 <zakaryan.2004@outlook.com>
2025-07-02 19:04:07 +0700

    fixup: Add more power menu customizations
    * Forward-port the rest of power menu customizations (airplane mode, screenshot, lockdown, bugreport, user switcher).
    * Screenshot option will always show by default and can be turned off in Settings.
    * Use the new LineageGlobalActionsService to get user's configuration
    
    Squashed with the following changes:
    
    Author: Michael Bestas <mikeioannina@gmail.com>
    Date:   Sat Jan 31 06:06:38 2015 +0200
    
        GlobalActions: Use circular user avatars
    
        * Use circular & smaller avatars, logic copied from SystemUI
        * Improve current user indication (thanks to maxwen)
    
        Change-Id: I81e6c4afd604f5cb8b22e2c4db9b174bbe7fb3f0
        Ticket-Id: OPO-456, CYNGNOS-1901
    
    Author: Gegham Zakaryan <zakaryan.2004@outlook.com>
    Date:   Mon Mar 29 20:17:47 2021 +0400
    
        frameworks: Add tunable for GlobalActions configuration
    
        Before, the power menu may have not updated when switching users.
        With tunables, we can be sure each user will only see their own settings.
    
        Remove the ACTION_UPDATE_POWER_MENU broadcast as well.
    
        Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
        Change-Id: I1d7dbe1eac41a83910c9c6db28162212971b42fb
    
    Author: Gegham Zakaryan <zakaryan.2004@outlook.com>
    Date:   Mon Mar 29 23:33:17 2021 +0400
    
        frameworks: New Users dialog for GlobalActions
    
        Take advantage of the new R-style dialog used in PowerOptions
        for showing the user switcher. All of the previous functionalities
        have been retained.
    
        Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
        Change-Id: Ib8ba9276d85b0ddcb3a188e72235eb77a4c70a82
    
    Author: Gegham Zakaryan <zakaryan.2004@outlook.com>
    Date:   Thu Apr 1 13:23:57 2021 +0200
    
        GlobalActionsDialog: Remove broken PhoneStateListener for airplane mode toggle
    
        The Airplane Mode toggle in GlobalActions is completely broken,
        being stuck in TurningOn transition state when pressing it when
        Airplane Mode is off. This happens because the PhoneStateListener
        doesn't trigger onServiceStateChanged when Airplane Mode turns on.
        This commit removes the redundant PhoneStateListener and makes the
        logic work for Telephony and non-Telephony devices pretty much the
        same way by allowing onAirplaneModeChanged() to handle both cases.
    
        Change-Id: Ifd1e06ff144addf8dd2c8494769b2d646599ae0a
        Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Fri Feb 25 23:49:46 2022 +0000
    
        GlobalActionsDialog: Never show emergency without telephony support
    
        Change-Id: I7a4cd340d69fa6e3e2e6a2ba0529276f3b04155b
    
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
    Change-Id: I225037422937602940aafb5d20acea8c0bcff6ba
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
@@ -1332,8 +1332,7 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
             super(R.drawable.ic_screenshot, R.string.global_action_screenshot);
         }
 
-        @Override
-        public void onPress() {
+        private void takeScreenshot(int type) {
             // Add a little delay before executing, to give the
             // dialog a chance to go away before it takes a
             // screenshot.
@@ -1349,9 +1348,13 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
         }
 
         @Override
+        public void onPress() {
+            takeScreenshot(TAKE_SCREENSHOT_FULLSCREEN);
+        }
+
+        @Override
         public boolean onLongPress() {
-            mScreenshotHelper.takeScreenshot(TAKE_SCREENSHOT_SELECTED_REGION, true, true,
-                    SCREENSHOT_GLOBAL_ACTIONS, mHandler, null);
+            takeScreenshot(TAKE_SCREENSHOT_SELECTED_REGION);
             return true;
         }
 
