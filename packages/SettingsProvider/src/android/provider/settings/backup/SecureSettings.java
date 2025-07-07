AOSP-Krypton/frameworks_base

base: allow toggling torch by double/long pressing power button
* This is acheived by extending existing gestures for camera / assistant, power menu open is not delayed.

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -194,5 +194,7 @@ public class SecureSettings {
         Settings.Secure.ENABLE_LOCATION_PRIVACY_INDICATOR,
         Settings.Secure.VOLUME_LINK_NOTIFICATION,
         Settings.Secure.GESTURE_NAVBAR_LENGTH_MODE,
+        Settings.Secure.TORCH_DOUBLE_TAP_POWER_GESTURE_ENABLED,
+        Settings.Secure.TORCH_LONG_PRESS_POWER,
     };
 }

services/core/java/com/android/server/GestureLauncherService.java
@@ -48,6 +48,7 @@ import com.android.internal.logging.UiEvent;
 import com.android.internal.logging.UiEventLogger;
 import com.android.internal.logging.UiEventLoggerImpl;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.krypton.KryptonUtils;
 import com.android.server.statusbar.StatusBarManagerInternal;
 import com.android.server.wm.WindowManagerInternal;
 
@@ -141,6 +142,11 @@ public class GestureLauncherService extends SystemService {
     private boolean mCameraDoubleTapPowerEnabled;
 
     /**
+     * Whether torch double tap power button gesture is currently enabled;
+     */
+    private boolean mTorchDoubleTapPowerEnabled;
+
+    /**
      * Whether emergency gesture is currently enabled
      */
     private boolean mEmergencyGestureEnabled;
@@ -225,6 +231,9 @@ public class GestureLauncherService extends SystemService {
                 Settings.Secure.getUriFor(Settings.Secure.CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED),
                 false, mSettingObserver, mUserId);
         mContext.getContentResolver().registerContentObserver(
+                Settings.Secure.getUriFor(Settings.Secure.TORCH_DOUBLE_TAP_POWER_GESTURE_ENABLED),
+                false, mSettingObserver, mUserId);
+        mContext.getContentResolver().registerContentObserver(
                 Settings.Secure.getUriFor(Settings.Secure.CAMERA_LIFT_TRIGGER_ENABLED),
                 false, mSettingObserver, mUserId);
         mContext.getContentResolver().registerContentObserver(
@@ -252,6 +261,14 @@ public class GestureLauncherService extends SystemService {
         boolean enabled = isCameraDoubleTapPowerSettingEnabled(mContext, mUserId);
         synchronized (this) {
             mCameraDoubleTapPowerEnabled = enabled;
+        }
+    }
+
+    private void updateTorchDoubleTapPowerEnabled() {
+        final boolean enabled = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+            Settings.Secure.TORCH_DOUBLE_TAP_POWER_GESTURE_ENABLED, 0, mUserId) == 1;
+        synchronized (this) {
+            mTorchDoubleTapPowerEnabled = enabled;
         }
     }
 
@@ -454,6 +471,7 @@ public class GestureLauncherService extends SystemService {
         boolean launchCamera = false;
         boolean launchEmergencyGesture = false;
         boolean intercept = false;
+        boolean toggleFlashlight = false;
         long powerTapInterval;
         synchronized (this) {
             powerTapInterval = event.getEventTime() - mLastPowerDown;
@@ -482,11 +500,15 @@ public class GestureLauncherService extends SystemService {
                     launchEmergencyGesture = true;
                 }
             }
-            if (mCameraDoubleTapPowerEnabled
-                    && powerTapInterval < CAMERA_POWER_DOUBLE_TAP_MAX_TIME_MS
+            if (powerTapInterval < CAMERA_POWER_DOUBLE_TAP_MAX_TIME_MS
                     && mPowerButtonConsecutiveTaps == CAMERA_POWER_TAP_COUNT_THRESHOLD) {
-                launchCamera = true;
-                intercept = interactive;
+                if (mCameraDoubleTapPowerEnabled) {
+                    launchCamera = true;
+                    intercept = interactive;
+                } else if (mTorchDoubleTapPowerEnabled) {
+                    toggleFlashlight = true;
+                    intercept = interactive;
+                }
             }
         }
         if (mPowerButtonConsecutiveTaps > 1 || mPowerButtonSlowConsecutiveTaps > 1) {
@@ -505,6 +527,8 @@ public class GestureLauncherService extends SystemService {
                         (int) powerTapInterval);
                 mUiEventLogger.log(GestureLauncherEvent.GESTURE_CAMERA_DOUBLE_TAP_POWER);
             }
+        } else if (toggleFlashlight) {
+            KryptonUtils.toggleCameraFlash();
         } else if (launchEmergencyGesture) {
             Slog.i(TAG, "Emergency gesture detected, launching.");
             launchEmergencyGesture = handleEmergencyGesture();
@@ -514,7 +538,7 @@ public class GestureLauncherService extends SystemService {
                 mPowerButtonSlowConsecutiveTaps);
         mMetricsLogger.histogram("power_double_tap_interval", (int) powerTapInterval);
 
-        outLaunched.value = launchCamera || launchEmergencyGesture;
+        outLaunched.value = launchCamera || toggleFlashlight || launchEmergencyGesture;
         // Intercept power key event if the press is part of a gesture (camera, eGesture) and the
         // user has completed setup.
         return intercept && isUserSetupComplete();
@@ -599,6 +623,7 @@ public class GestureLauncherService extends SystemService {
                 registerContentObservers();
                 updateCameraRegistered();
                 updateCameraDoubleTapPowerEnabled();
+                updateTorchDoubleTapPowerEnabled();
                 updateEmergencyGestureEnabled();
             }
         }
@@ -609,6 +634,7 @@ public class GestureLauncherService extends SystemService {
             if (userId == mUserId) {
                 updateCameraRegistered();
                 updateCameraDoubleTapPowerEnabled();
+                updateTorchDoubleTapPowerEnabled();
                 updateEmergencyGestureEnabled();
             }
         }

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -206,6 +206,7 @@ import com.android.internal.policy.PhoneWindow;
 import com.android.internal.policy.TransitionAnimation;
 import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.util.ArrayUtils;
+import com.android.internal.util.krypton.KryptonUtils;
 import com.android.server.ExtconStateObserver;
 import com.android.server.ExtconUEventObserver;
 import com.android.server.GestureLauncherService;
@@ -490,7 +491,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     int mLidNavigationAccessibility;
     int mShortPressOnPowerBehavior;
     int mLongPressOnPowerBehavior;
-    long mLongPressOnPowerAssistantTimeoutMs;
+    private boolean mTorchLongPressPowerEnabled;
+    long mLongPressOnPowerTimeoutMs;
     int mVeryLongPressOnPowerBehavior;
     int mDoublePressOnPowerBehavior;
     int mTriplePressOnPowerBehavior;
@@ -767,6 +769,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.VOLUME_BUTTON_MUSIC_CONTROL), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.TORCH_LONG_PRESS_POWER), false, this,
                     UserHandle.USER_ALL);
             updateSettings();
         }
@@ -1150,6 +1155,25 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         AssistUtils.INVOCATION_TYPE_POWER_BUTTON_LONG_PRESS);
                 break;
         }
+    }
+
+    private boolean powerLongPressGestures(long eventTime, boolean nonInteractive) {
+        if (DEBUG_INPUT) Slog.d(TAG, "powerLongPressGestures: eventTime = " + eventTime
+                + ", nonInteractive = " + nonInteractive +
+                ", mTorchLongPressPowerEnabled = " + mTorchLongPressPowerEnabled);
+        if (mTorchLongPressPowerEnabled && (nonInteractive || isFlashlightOn())) {
+            performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, false,
+                    "Power - Long Press - Toggle flashlight");
+            KryptonUtils.toggleCameraFlash();
+            mPowerKeyHandled = true;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isFlashlightOn() {
+        return Settings.Secure.getInt(mContext.getContentResolver(),
+            Settings.Secure.FLASHLIGHT_ENABLED, 0) == 1;
     }
 
     private void powerVeryLongPress() {
@@ -1763,7 +1787,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 com.android.internal.R.integer.config_shortPressOnPowerBehavior);
         mLongPressOnPowerBehavior = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_longPressOnPowerBehavior);
-        mLongPressOnPowerAssistantTimeoutMs = mContext.getResources().getInteger(
+        mLongPressOnPowerTimeoutMs = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_longPressOnPowerDurationMs);
         mVeryLongPressOnPowerBehavior = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_veryLongPressOnPowerBehavior);
@@ -2027,8 +2051,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         @Override
         long getLongPressTimeoutMs() {
-            if (getResolvedLongPressOnPowerBehavior() == LONG_PRESS_POWER_ASSISTANT) {
-                return mLongPressOnPowerAssistantTimeoutMs;
+            if (getResolvedLongPressOnPowerBehavior() == LONG_PRESS_POWER_ASSISTANT
+                    || mTorchLongPressPowerEnabled) {
+                return mLongPressOnPowerTimeoutMs;
             } else {
                 return super.getLongPressTimeoutMs();
             }
@@ -2036,11 +2061,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         @Override
         void onLongPress(long eventTime) {
-            if (mSingleKeyGestureDetector.beganFromNonInteractive()
-                    && !mSupportLongPressPowerWhenNonInteractive) {
+            final boolean nonInteractive = mSingleKeyGestureDetector.beganFromNonInteractive();
+            if (nonInteractive && !mSupportLongPressPowerWhenNonInteractive) {
                 Slog.v(TAG, "Not support long press power when device is not interactive.");
                 return;
             }
+            if (powerLongPressGestures(eventTime, nonInteractive)) return;
 
             powerLongPress(eventTime);
         }
@@ -2184,7 +2210,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     Settings.Global.POWER_BUTTON_LONG_PRESS,
                     mContext.getResources().getInteger(
                             com.android.internal.R.integer.config_longPressOnPowerBehavior));
-            mLongPressOnPowerAssistantTimeoutMs = Settings.Global.getLong(
+            mLongPressOnPowerTimeoutMs = Settings.Global.getLong(
                     mContext.getContentResolver(),
                     Settings.Global.POWER_BUTTON_LONG_PRESS_DURATION_MS,
                     mContext.getResources().getInteger(
@@ -2197,6 +2223,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     Settings.Global.KEY_CHORD_POWER_VOLUME_UP,
                     mContext.getResources().getInteger(
                             com.android.internal.R.integer.config_keyChordPowerVolumeUp));
+            mTorchLongPressPowerEnabled = Settings.Secure.getInt(resolver,
+                    Settings.Secure.TORCH_LONG_PRESS_POWER, 0) == 1;
         }
         if (updateRotation) {
             updateRotation(true);
@@ -5467,8 +5495,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 pw.print("mLongPressOnPowerBehavior=");
                 pw.println(longPressOnPowerBehaviorToString(mLongPressOnPowerBehavior));
         pw.print(prefix);
-        pw.print("mLongPressOnPowerAssistantTimeoutMs=");
-        pw.println(mLongPressOnPowerAssistantTimeoutMs);
+                pw.print("mTorchLongPressPowerEnabled=");
+                pw.println(String.valueOf(mTorchLongPressPowerEnabled));
+        pw.print(prefix);
+        pw.print("mLongPressOnPowerTimeoutMs=");
+        pw.println(mLongPressOnPowerTimeoutMs);
         pw.print(prefix);
                 pw.print("mVeryLongPressOnPowerBehavior=");
                 pw.println(veryLongPressOnPowerBehaviorToString(mVeryLongPressOnPowerBehavior));

CherishOS/platform_frameworks_base

Signed-off-by: Dmitrii <bankersenator@gmail.com>
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -304,6 +304,8 @@ public class SecureSettings {
         Settings.Secure.SELECTED_SPELL_CHECKER,
         // SELECTED_SPELL_CHECKER_SUBTYPE needs to be restored after SELECTED_SPELL_CHECKER
         Settings.Secure.SELECTED_SPELL_CHECKER_SUBTYPE,
-        Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS
+        Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
+        Settings.Secure.TORCH_DOUBLE_TAP_POWER_GESTURE_ENABLED,
+        Settings.Secure.TORCH_LONG_PRESS_POWER
     };
 }

services/core/java/com/android/server/GestureLauncherService.java
@@ -59,6 +59,7 @@ import com.android.internal.logging.UiEvent;
 import com.android.internal.logging.UiEventLogger;
 import com.android.internal.logging.UiEventLoggerImpl;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.cherish.CherishUtils;
 import com.android.server.statusbar.StatusBarManagerInternal;
 import com.android.server.wm.WindowManagerInternal;
 
@@ -194,6 +195,11 @@ public class GestureLauncherService extends SystemService {
     private boolean mWalletDoubleTapPowerEnabled;
 
     /**
+     * Whether torch double tap power button gesture is currently enabled;
+     */
+    private boolean mTorchDoubleTapPowerEnabled;
+
+    /**
      * Whether emergency gesture is currently enabled
      */
     private boolean mEmergencyGestureEnabled;
@@ -309,6 +315,9 @@ public class GestureLauncherService extends SystemService {
                         Settings.Secure.CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED),
                 false, mSettingObserver, mUserId);
         mContext.getContentResolver().registerContentObserver(
+                Settings.Secure.getUriFor(Settings.Secure.TORCH_DOUBLE_TAP_POWER_GESTURE_ENABLED),
+                false, mSettingObserver, mUserId);
+        mContext.getContentResolver().registerContentObserver(
                 Settings.Secure.getUriFor(Settings.Secure.CAMERA_LIFT_TRIGGER_ENABLED),
                 false, mSettingObserver, mUserId);
         mContext.getContentResolver().registerContentObserver(
@@ -340,6 +349,14 @@ public class GestureLauncherService extends SystemService {
         boolean enabled = isCameraDoubleTapPowerSettingEnabled(mContext, mUserId);
         synchronized (this) {
             mCameraDoubleTapPowerEnabled = enabled;
+        }
+    }
+
+    private void updateTorchDoubleTapPowerEnabled() {
+        final boolean enabled = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+            Settings.Secure.TORCH_DOUBLE_TAP_POWER_GESTURE_ENABLED, 0, mUserId) == 1;
+        synchronized (this) {
+            mTorchDoubleTapPowerEnabled = enabled;
         }
     }
 
@@ -666,6 +683,7 @@ public class GestureLauncherService extends SystemService {
         boolean launchWallet = false;
         boolean launchEmergencyGesture = false;
         boolean intercept = false;
+        boolean toggleFlashlight = false;
         long powerTapInterval;
         synchronized (this) {
             powerTapInterval = event.getEventTime() - mLastPowerDown;
@@ -719,19 +737,21 @@ public class GestureLauncherService extends SystemService {
                     }
                 }
             }
-            if (mCameraDoubleTapPowerEnabled
-                    && powerTapInterval < POWER_DOUBLE_TAP_MAX_TIME_MS
+            if (powerTapInterval < POWER_DOUBLE_TAP_MAX_TIME_MS
                     && mPowerButtonConsecutiveTaps == DOUBLE_POWER_TAP_COUNT_THRESHOLD) {
-                launchCamera = true;
-                intercept = interactive;
-            } else if (launchWalletOptionOnPowerDoubleTap()
-                    && mWalletDoubleTapPowerEnabled
-                    && powerTapInterval < POWER_DOUBLE_TAP_MAX_TIME_MS
-                    && mPowerButtonConsecutiveTaps == DOUBLE_POWER_TAP_COUNT_THRESHOLD) {
-                launchWallet = true;
-                intercept = interactive;
+                if (mCameraDoubleTapPowerEnabled) {
+                    launchCamera = true;
+                    intercept = interactive;
+                } else if (mTorchDoubleTapPowerEnabled) {
+                    toggleFlashlight = true;
+                    intercept = interactive;
+                } else if (launchWalletOptionOnPowerDoubleTap() && mWalletDoubleTapPowerEnabled) {
+                    launchWallet = true;
+                    intercept = interactive;
+                }
             }
         }
+
         if (mPowerButtonConsecutiveTaps > 1 || mPowerButtonSlowConsecutiveTaps > 1) {
             Slog.i(TAG, Long.valueOf(mPowerButtonConsecutiveTaps)
                     + " consecutive power button taps detected, "
@@ -753,6 +773,8 @@ public class GestureLauncherService extends SystemService {
                     + powerTapInterval + "ms");
             launchWallet = launchWalletViaSysuiCallbacks() ?
                     handleWalletGesture() : sendGestureTargetActivityPendingIntent();
+        } else if (toggleFlashlight) {
+            CherishUtils.toggleCameraFlash();
         } else if (launchEmergencyGesture) {
             Slog.i(TAG, "Emergency gesture detected, launching.");
             launchEmergencyGesture = handleEmergencyGesture();
@@ -768,7 +790,7 @@ public class GestureLauncherService extends SystemService {
                 mPowerButtonSlowConsecutiveTaps);
         mMetricsLogger.histogram("power_double_tap_interval", (int) powerTapInterval);
 
-        outLaunched.value = launchCamera || launchEmergencyGesture || launchWallet;
+        outLaunched.value = launchCamera || toggleFlashlight || launchEmergencyGesture || launchWallet;
         // Intercept power key event if the press is part of a gesture (camera, eGesture) and the
         // user has completed setup.
         return intercept && isUserSetupComplete();
@@ -943,6 +965,7 @@ public class GestureLauncherService extends SystemService {
                 registerContentObservers();
                 updateCameraRegistered();
                 updateCameraDoubleTapPowerEnabled();
+                updateTorchDoubleTapPowerEnabled();
                 if (launchWalletOptionOnPowerDoubleTap()) {
                     updateWalletDoubleTapPowerEnabled();
                 }
@@ -957,6 +980,7 @@ public class GestureLauncherService extends SystemService {
             if (userId == mUserId) {
                 updateCameraRegistered();
                 updateCameraDoubleTapPowerEnabled();
+                updateTorchDoubleTapPowerEnabled();
                 if (launchWalletOptionOnPowerDoubleTap()) {
                     updateWalletDoubleTapPowerEnabled();
                 }

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -247,6 +247,7 @@ import com.android.internal.policy.PhoneWindow;
 import com.android.internal.policy.TransitionAnimation;
 import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.widget.LockPatternUtils;
+import com.android.internal.util.cherish.CherishUtils;
 import com.android.server.AccessibilityManagerInternal;
 import com.android.server.DockObserverInternal;
 import com.android.server.ExtconStateObserver;
@@ -630,7 +631,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private boolean mShouldEarlyShortPressOnPower;
     boolean mShouldEarlyShortPressOnStemPrimary;
     int mLongPressOnPowerBehavior;
-    long mLongPressOnPowerAssistantTimeoutMs;
+    private boolean mTorchLongPressPowerEnabled;
+    long mLongPressOnPowerTimeoutMs;
     int mVeryLongPressOnPowerBehavior;
     int mDoublePressOnPowerBehavior;
     ComponentName mPowerDoublePressTargetActivity;
@@ -1122,6 +1124,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         Settings.Secure.SWAP_CAPACITIVE_KEYS), false, this,
                         UserHandle.USER_ALL);
             }
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.TORCH_LONG_PRESS_POWER), false, this,
+                    UserHandle.USER_ALL);
             updateSettings();
         }
 
@@ -1734,6 +1739,25 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 mPocketManager.setListeningExternal(false);
                 break;
         }
+    }
+
+    private boolean powerLongPressGestures(long eventTime, boolean nonInteractive) {
+        if (DEBUG_INPUT) Slog.d(TAG, "powerLongPressGestures: eventTime = " + eventTime
+                + ", nonInteractive = " + nonInteractive +
+                ", mTorchLongPressPowerEnabled = " + mTorchLongPressPowerEnabled);
+        if (mTorchLongPressPowerEnabled && (nonInteractive || isFlashlightOn())) {
+            performHapticFeedback(HapticFeedbackConstants.LONG_PRESS,
+                    "Power - Long Press - Toggle flashlight");
+            CherishUtils.toggleCameraFlash();
+            mPowerKeyHandled = true;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isFlashlightOn() {
+        return Settings.Secure.getInt(mContext.getContentResolver(),
+            Settings.Secure.FLASHLIGHT_ENABLED, 0) == 1;
     }
 
     private void powerVeryLongPress() {
@@ -2695,7 +2719,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 com.android.internal.R.integer.config_shortPressOnPowerBehavior);
         mLongPressOnPowerBehavior = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_longPressOnPowerBehavior);
-        mLongPressOnPowerAssistantTimeoutMs = mContext.getResources().getInteger(
+        mLongPressOnPowerTimeoutMs = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_longPressOnPowerDurationMs);
         mVeryLongPressOnPowerBehavior = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_veryLongPressOnPowerBehavior);
@@ -3017,8 +3041,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         @Override
         long getLongPressTimeoutMs() {
-            if (getResolvedLongPressOnPowerBehavior() == LONG_PRESS_POWER_ASSISTANT) {
-                return mLongPressOnPowerAssistantTimeoutMs;
+            if (getResolvedLongPressOnPowerBehavior() == LONG_PRESS_POWER_ASSISTANT
+                    || mTorchLongPressPowerEnabled) {
+                return mLongPressOnPowerTimeoutMs;
             } else {
                 return super.getLongPressTimeoutMs();
             }
@@ -3026,11 +3051,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         @Override
         void onLongPress(long eventTime) {
-            if (mSingleKeyGestureDetector.beganFromNonInteractive()
-                    && !mSupportLongPressPowerWhenNonInteractive) {
+            final boolean nonInteractive = mSingleKeyGestureDetector.beganFromNonInteractive();
+            if (nonInteractive && !mSupportLongPressPowerWhenNonInteractive) {
                 Slog.v(TAG, "Not support long press power when device is not interactive.");
                 return;
             }
+            if (powerLongPressGestures(eventTime, nonInteractive)) return;
 
             powerLongPress(eventTime);
         }
@@ -3538,7 +3564,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 mVeryLongPressOnPowerBehavior = veryLongPressOnPowerBehavior;
             }
 
-            mLongPressOnPowerAssistantTimeoutMs = Settings.Global.getLong(
+            mLongPressOnPowerTimeoutMs = Settings.Global.getLong(
                     mContext.getContentResolver(),
                     Settings.Global.POWER_BUTTON_LONG_PRESS_DURATION_MS,
                     mContext.getResources().getInteger(
@@ -3576,6 +3602,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mStylusButtonsEnabled = Settings.Secure.getIntForUser(resolver,
                     Secure.STYLUS_BUTTONS_ENABLED, 1, UserHandle.USER_CURRENT) == 1;
             mInputManagerInternal.setStylusButtonMotionEventsEnabled(mStylusButtonsEnabled);
+
+            mTorchLongPressPowerEnabled = Settings.Secure.getIntForUser(resolver,
+                    Settings.Secure.TORCH_LONG_PRESS_POWER, 0,
+                    UserHandle.USER_CURRENT) == 1;
 
             kidsModeEnabled = Settings.Secure.getIntForUser(resolver,
                     Settings.Secure.NAV_BAR_KIDS_MODE, 0, UserHandle.USER_CURRENT) == 1;
@@ -7975,8 +8005,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         pw.print("mSettingsKeyBehavior=");
         pw.println(settingsKeyBehaviorToString(mSettingsKeyBehavior));
         pw.print(prefix);
-        pw.print("mLongPressOnPowerAssistantTimeoutMs=");
-        pw.println(mLongPressOnPowerAssistantTimeoutMs);
+                pw.print("mTorchLongPressPowerEnabled=");
+                pw.println(String.valueOf(mTorchLongPressPowerEnabled));
+        pw.print(prefix);
+        pw.print("mLongPressOnPowerTimeoutMs=");
+        pw.println(mLongPressOnPowerTimeoutMs);
         pw.print(prefix);
                 pw.print("mVeryLongPressOnPowerBehavior=");
                 pw.println(veryLongPressOnPowerBehaviorToString(mVeryLongPressOnPowerBehavior));