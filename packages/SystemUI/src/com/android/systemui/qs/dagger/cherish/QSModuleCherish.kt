963795826d854f7b98a75356855b1e0a58c09e79
darkobas <darkobas@gmail.com>
2025-07-08 22:16:51 +0700

    SystemUI: FPS Info Overlay & Tile
    @/neobuddy89: Adapted tile for A13, A14.
    shuttercat: add in DISPLAY group
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/dagger/cherish/QSModuleCherish.kt
+++ packages/SystemUI/src/com/android/systemui/qs/dagger/cherish/QSModuleCherish.kt
@@ -21,6 +21,7 @@ import com.android.systemui.qs.QsEventLogger
 import com.android.systemui.qs.pipeline.shared.TileSpec
 import com.android.systemui.qs.shared.model.TileCategory
 import com.android.systemui.qs.tileimpl.QSTileImpl
+import com.android.systemui.qs.tiles.FPSInfoTile
 import com.android.systemui.qs.tiles.AmbientDisplayTile
 import com.android.systemui.qs.tiles.AODTile
 import com.android.systemui.qs.tiles.CellularTile
@@ -98,6 +99,12 @@ interface QSModuleCherish {
     @IntoMap
     @StringKey(CPUInfoTile.TILE_SPEC)
     fun CPUInfoTile(cpuInfoTile: CPUInfoTile): QSTileImpl<*>
+
+    /** Inject FPSInfoTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(FPSInfoTile.TILE_SPEC)
+    fun FPSInfoTile(fpsInfoTile: FPSInfoTile): QSTileImpl<*>
 
     companion object {
 
@@ -249,6 +256,21 @@ interface QSModuleCherish {
                 ),
                 instanceId = uiEventLogger.getNewInstanceId(),
                 category = TileCategory.UTILITIES
+            )
+        }
+
+       @Provides
+        @IntoMap
+        @StringKey(FPSInfoTile.TILE_SPEC)
+        fun provideFPSInfoConfig(uiEventLogger: QsEventLogger): QSTileConfig {
+            return QSTileConfig(
+                tileSpec = TileSpec.create(FPSInfoTile.TILE_SPEC),
+                uiConfig = QSTileUIConfig.Resource(
+                    iconRes = R.drawable.ic_qs_fps_info,
+                    labelRes = R.string.quick_settings_fpsinfo_label
+                ),
+                instanceId = uiEventLogger.getNewInstanceId(),
+                category = TileCategory.DISPLAY
             )
         }
     }
