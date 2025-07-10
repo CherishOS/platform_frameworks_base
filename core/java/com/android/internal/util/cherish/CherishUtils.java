7d7bac297b2fd38773c69f2835864c873e653f42
Hưng Phan <phandinhhungvp2001@gmail.com>
2025-07-10 23:19:06 +0700

    Revert "Utils: Add method to detect whether overlay is enabled"
    This reverts commit b52b95e2d22abc2f7abe0d2aa11df71d1fee6bd6.

--- core/java/com/android/internal/util/cherish/CherishUtils.java
+++ core/java/com/android/internal/util/cherish/CherishUtils.java
@@ -26,8 +26,6 @@ import android.app.IActivityManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.Resources;
-import android.content.om.IOverlayManager;
-import android.content.om.OverlayInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -413,42 +411,6 @@ public class CherishUtils {
             }
         }
         return NO_CUTOUT;
-    }
-
-    // Method to detect whether an overlay is enabled or not
-    public static boolean isThemeEnabled(String packageName) {
-        mOverlayService = new OverlayManager();
-        try {
-            List<OverlayInfo> infos = mOverlayService.getOverlayInfosForTarget("android",
-                    UserHandle.myUserId());
-            for (int i = 0, size = infos.size(); i < size; i++) {
-                if (infos.get(i).packageName.equals(packageName)) {
-                    return infos.get(i).isEnabled();
-                }
-            }
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    public static class OverlayManager {
-        private final IOverlayManager mService;
-
-        public OverlayManager() {
-            mService = IOverlayManager.Stub.asInterface(
-                    ServiceManager.getService(Context.OVERLAY_SERVICE));
-        }
-
-        public void setEnabled(String pkg, boolean enabled, int userId)
-                throws RemoteException {
-            mService.setEnabled(pkg, enabled, userId);
-        }
-
-        public List<OverlayInfo> getOverlayInfosForTarget(String target, int userId)
-                throws RemoteException {
-            return mService.getOverlayInfosForTarget(target, userId);
-        }
     }
 
     public static void restartSystemUi(Context context) {
