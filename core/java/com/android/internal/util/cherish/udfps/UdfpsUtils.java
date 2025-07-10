dcb60d73bd2491a56f7e80e0d794cc66a0f2dd0e
Hưng Phan <phandinhhungvp2001@gmail.com>
2025-07-10 19:44:00 +0700

    Update overlay UdfpsUtils with A16
    Add overlay in your device https://github.com/hungphan2001/device_xiaomi_raphael/commit/7d14bf887527cc2ba472e52c9fd2e5c8755b3d01
    It will show Screen-off udfps unlock
    udfps animation and udfps icon picker
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

--- core/java/com/android/internal/util/cherish/udfps/UdfpsUtils.java
+++ core/java/com/android/internal/util/cherish/udfps/UdfpsUtils.java
@@ -17,13 +17,10 @@ package com.android.internal.util.cherish.udfps;
 
 import android.content.Context;
 
-import com.android.internal.util.ArrayUtils;
-
 public class UdfpsUtils {
     public static boolean hasUdfpsSupport(Context context) {
-        int[] udfpsProps = context.getResources().getIntArray(
-                com.android.internal.R.array.config_udfps_sensor_props);
-
-        return !ArrayUtils.isEmpty(udfpsProps);
+        boolean udfpsProps = context.getResources().getBoolean(
+                com.android.internal.R.bool.config_screen_off_udfps_enabled);
+        return udfpsProps ;
     }
 }
