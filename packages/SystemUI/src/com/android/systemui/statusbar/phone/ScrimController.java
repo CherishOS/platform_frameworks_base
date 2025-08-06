43abc4204f8b32a18024478b21a049b99e0e87fd
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-06 15:55:10 +0800

    [BUGFIX] fix boot failure with white wallpaper
    Change-Id: I906c2335a08e6ac43a40ff0b1411c900b2f0a874
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -1210,7 +1210,9 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
         setOrAdaptCurrentAnimation(mScrimBehind);
         setOrAdaptCurrentAnimation(mNotificationsScrim);
         setOrAdaptCurrentAnimation(mScrimInFront);
-        dispatchBackScrimState(mScrimBehind.getViewAlpha());
+        if (mScrimBehind != null) {
+            dispatchBackScrimState(mScrimBehind.getViewAlpha());
+        }
         com.android.systemui.util.ScrimUtils.getInstance(mScrimBehind.getContext()).onScrimDispatched();
     }
 
