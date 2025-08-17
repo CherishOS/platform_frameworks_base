c44f2f0628b8af0d9678d94e9739ab6f110532b5
Pranav Vashi <neobuddy89@gmail.com>
2025-08-18 02:20:19 +0530

    SystemUI: Add customization to select QS tile shape
    * At present it checks only for the state. Inject setting to pre-select shape.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7551,6 +7551,12 @@ public final class Settings {
         public static final String SECURE_LOCKSCREEN_QS_DISABLED = "secure_lockscreen_qs_disabled";
 
         /**
+         * Customize QS tile shape.
+         * @hide
+         */
+        public static final String QS_TILE_SHAPE = "qs_tile_shape";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
