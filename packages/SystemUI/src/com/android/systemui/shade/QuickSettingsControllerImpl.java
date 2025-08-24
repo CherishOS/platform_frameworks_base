821e8c3d77148fe2c457370af5ceaaca94af44a5
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-24 09:07:30 +0800

    [FEATURE] notification row transparency switch
    Change-Id: I4ea5385caf72336bd8fbc87f85e3e00e85d189d2
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/QuickSettingsControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/shade/QuickSettingsControllerImpl.java
@@ -33,16 +33,20 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
 import android.app.Fragment;
+import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.graphics.Insets;
 import android.graphics.Rect;
 import android.graphics.Region;
+import android.os.Handler;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.IndentingPrintWriter;
 import android.util.Log;
 import android.util.MathUtils;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
+import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.view.WindowInsets;
@@ -87,6 +91,7 @@ import com.android.systemui.statusbar.PulseExpansionHandler;
 import com.android.systemui.statusbar.QsFrameTranslateController;
 import com.android.systemui.statusbar.StatusBarState;
 import com.android.systemui.statusbar.notification.domain.interactor.ActiveNotificationsInteractor;
+import com.android.systemui.statusbar.notification.row.ExpandableNotificationRow;
 import com.android.systemui.statusbar.notification.stack.AmbientState;
 import com.android.systemui.statusbar.notification.stack.NotificationStackScrollLayout;
 import com.android.systemui.statusbar.notification.stack.NotificationStackScrollLayoutController;
@@ -288,6 +293,8 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
 
     private int mOneFingerQuickSettingsIntercept;
 
+    private final ContentObserver mTranslucentObserver;
+    
     private final Region mInterceptRegion = new Region();
     /** The end bounds of a clipping animation. */
     private final Rect mClippingAnimationEndBounds = new Rect();
@@ -402,7 +409,15 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
         dumpManager.registerDumpable(this);
 
         mWindowManagerProvider = windowManagerProvider;
+
         mTunerService = tunerService;
+
+        mTranslucentObserver = new ContentObserver(null) {
+            @Override
+            public void onChange(boolean selfChange) {
+                onTransparencyUpdated();
+            }
+        };
     }
 
     @VisibleForTesting
@@ -2242,6 +2257,14 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
             }
             mQs.setScrollListener(mQsScrollListener);
             mTunerService.addTunable(this, STATUS_BAR_QUICK_QS_PULLDOWN);
+
+            mPanelView.getContext().getContentResolver().registerContentObserver(
+                    Settings.Secure.getUriFor(
+                            "notification_row_transparency"),
+                    true, mTranslucentObserver,
+                    UserHandle.USER_ALL);
+
+            mTranslucentObserver.onChange(true);
             updateExpansion();
         }
 
@@ -2249,6 +2272,8 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
         @Override
         public void onFragmentViewDestroyed(String tag, Fragment fragment) {
             mTunerService.removeTunable(this);
+            mPanelView.getContext().getContentResolver().unregisterContentObserver(
+                    mTranslucentObserver);
             // Manual handling of fragment lifecycle is only required because this bridges
             // non-fragment and fragment code. Once we are using a fragment for the notification
             // panel, mQs will not need to be null cause it will be tied to the same lifecycle.
@@ -2441,5 +2466,21 @@ public class QuickSettingsControllerImpl implements QuickSettingsController, Dum
     interface FlingQsWithoutClickListener {
         void onFlingQsWithoutClick(ValueAnimator animator, float qsExpansionHeight,
                 float target, float vel);
+    }
+
+    public final void onTransparencyUpdated() {
+        NotificationStackScrollLayoutController controller = mNotificationStackScrollLayoutController;
+        if (controller == null || controller.getView() == null) {
+            return;
+        }
+        NotificationStackScrollLayout view = controller.getView();
+        int childCount = view.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = view.getChildAt(i);
+            if (child instanceof ExpandableNotificationRow) {
+                final ExpandableNotificationRow row = (ExpandableNotificationRow) child;
+                child.post(row::updateIfNeeded);
+            }
+        }
     }
 }
