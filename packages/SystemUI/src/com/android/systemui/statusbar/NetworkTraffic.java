7758592739f8ee360cb7f2cd3179f3e0056bd345
Pranav Vashi <neobuddy89@gmail.com>
2025-09-29 07:47:26 +0530

    SystemUI: NetworkTraffic: Respect layout direction for text
    Also -
    * Fix potential leak in KeyguardUpdateMonitorCallback and clearHandlerCallbacks.
    * Fix tinting for arrows to match dark/light properly.
    
    Ref: https://github.com/crdroidandroid/issue_tracker/issues/790
    Change-Id: Idd2b570964174febcf77756681c4158cce10229a
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/NetworkTraffic.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NetworkTraffic.java
@@ -42,6 +42,8 @@ import android.util.AttributeSet;
 import android.view.Gravity;
 import android.widget.TextView;
 
+import androidx.core.content.res.ResourcesCompat;
+
 import com.android.systemui.Dependency;
 import com.android.systemui.res.R;
 import com.android.systemui.tuner.TunerService;
@@ -397,10 +399,16 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
         super.onDetachedFromWindow();
         if (mAttached) {
             clearHandlerCallbacks();
+            if (mKeyguardUpdateMonitor != null) {
+                mKeyguardUpdateMonitor.removeCallback(mUpdateCallback);
+                mKeyguardUpdateMonitor = null;
+            }
             mContext.unregisterReceiver(mIntentReceiver);
             mConnectivityManager.unregisterNetworkCallback(mDefaultNetworkCallback);
             mConnectivityManager.unregisterNetworkCallback(mNetworkCallback);
             Dependency.get(TunerService.class).removeTunable(this);
+            mDrawable = null;
+            setCompoundDrawables(null, null, null, null);
             mAttached = false;
         }
     }
@@ -449,6 +457,9 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
                     String txtFont = getResources().getString(com.android.internal.R.string.config_bodyFontFamily);
                     setTypeface(Typeface.create(txtFont, Typeface.BOLD));
                     setLineSpacing(0.80f, 0.80f);
+                    setLayoutDirection(View.LAYOUT_DIRECTION_LOCALE);
+                    setTextDirection(View.TEXT_DIRECTION_LOCALE);
+                    setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
                 }
                 updateViews();
                 break;
@@ -506,14 +517,11 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
     }
 
     private void clearHandlerCallbacks() {
-        mTrafficHandler.removeMessages(MESSAGE_TYPE_PERIODIC_REFRESH);
-        mTrafficHandler.removeMessages(MESSAGE_TYPE_UPDATE_VIEW);
+        mTrafficHandler.removeCallbacksAndMessages(null);
     }
 
     private void setTrafficDrawable() {
         final int drawableResId;
-        final Drawable drawable;
-
         if (mHideArrows) {
             drawableResId = 0;
         } else if (!mTrafficActive) {
@@ -527,11 +535,12 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
         } else {
             drawableResId = 0;
         }
-        drawable = drawableResId != 0 ? getResources().getDrawable(drawableResId) : null;
+        final Drawable drawable = mHideArrows ? null
+            : ResourcesCompat.getDrawable(getResources(), drawableResId, getContext().getTheme());
         if (mDrawable != drawable || mIconTint != newTint) {
             mDrawable = drawable;
             mIconTint = newTint;
-            setCompoundDrawablesWithIntrinsicBounds(null, null, mDrawable, null);
+            setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, mDrawable, null);
             updateTrafficDrawable();
         }
     }
