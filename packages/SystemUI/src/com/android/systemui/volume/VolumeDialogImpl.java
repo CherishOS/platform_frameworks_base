crdroidandroid/android_frameworks_base

SystemUI: VolumeDialog: Fix volume panel on left on secondary users
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -436,9 +436,10 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
             ContentObserver volumePanelOnLeftObserver = new ContentObserver(null) {
                 @Override
                 public void onChange(boolean selfChange) {
-                    final boolean volumePanelOnLeft = LineageSettings.Secure.getInt(
+                    final boolean volumePanelOnLeft = LineageSettings.Secure.getIntForUser(
                             mContext.getContentResolver(),
-                            LineageSettings.Secure.VOLUME_PANEL_ON_LEFT, 0) != 0;
+                            LineageSettings.Secure.VOLUME_PANEL_ON_LEFT, 0,
+                            UserHandle.USER_CURRENT) != 0;
                     if (mVolumePanelOnLeft != volumePanelOnLeft) {
                         mVolumePanelOnLeft = volumePanelOnLeft;
                         mHandler.post(mControllerCallbackH::onConfigurationChanged);

CherishOS/platform_frameworks_base

Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -453,9 +453,10 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
             ContentObserver volumePanelOnLeftObserver = new ContentObserver(null) {
                 @Override
                 public void onChange(boolean selfChange) {
-                    final boolean volumePanelOnLeft = Settings.Secure.getInt(
+                    final boolean volumePanelOnLeft = Settings.Secure.getIntForUser(
                             mContext.getContentResolver(),
-                            Settings.Secure.VOLUME_PANEL_ON_LEFT, 0) != 0;
+                            Settings.Secure.VOLUME_PANEL_ON_LEFT, 0,
+                            UserHandle.USER_CURRENT) != 0;
                     if (mVolumePanelOnLeft != volumePanelOnLeft) {
                         mVolumePanelOnLeft = volumePanelOnLeft;
                         mHandler.post(mControllerCallbackH::onConfigurationChanged);