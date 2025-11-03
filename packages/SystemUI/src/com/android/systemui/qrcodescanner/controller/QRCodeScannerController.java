crdroidandroid/android_frameworks_base

QRCodeScannerController: Check for google package availability
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/qrcodescanner/controller/QRCodeScannerController.java
@@ -32,6 +32,7 @@ import android.provider.Settings;
 import android.util.Log;
 
 import com.android.internal.config.sysui.SystemUiDeviceConfigFlags;
+import com.android.internal.util.crdroid.Utils;
 import com.android.systemui.dagger.SysUISingleton;
 import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.settings.UserTracker;
@@ -278,7 +279,7 @@ public class QRCodeScannerController implements
         bundle.putString("caller_package", GSA_PACKAGE);
         bundle.putLong("start_activity_time_nanos", SystemClock.elapsedRealtimeNanos());
         intent.setComponent(new ComponentName(GSA_PACKAGE, LENS_ACTIVITY))
-                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK)
                 .setPackage(GSA_PACKAGE)
                 .setData(Uri.parse("google://lens"))
                 .putExtra("lens_activity_params", bundle);
@@ -300,18 +301,19 @@ public class QRCodeScannerController implements
         String prevQrCodeScannerActivity = mQRCodeScannerActivity;
         ComponentName componentName = null;
         Intent intent = new Intent();
-        if (qrCodeScannerActivity != null) {
+        if (qrCodeScannerActivity != null && !qrCodeScannerActivity.isEmpty()) {
             componentName = ComponentName.unflattenFromString(qrCodeScannerActivity);
             intent.setComponent(componentName);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
         }
 
         Intent lensIntent = getLensIntent();
-        if (isActivityAvailable(intent)) {
+        if (intent != null && isActivityAvailable(intent)) {
             mQRCodeScannerActivity = qrCodeScannerActivity;
             mComponentName = componentName;
             mIntent = intent;
-        } else if (isActivityCallable(lensIntent)) {
+        } else if (Utils. isPackageInstalled(mContext, GSA_PACKAGE, false) &&
+                lensIntent != null && isActivityCallable(lensIntent)) {
             mQRCodeScannerActivity = LENS_ACTIVITY;
             mComponentName = lensIntent.getComponent();
             mIntent = lensIntent;

CherishOS/platform_frameworks_base

Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

packages/SystemUI/src/com/android/systemui/qrcodescanner/controller/QRCodeScannerController.java
@@ -32,6 +32,7 @@ import android.provider.Settings;
 import android.util.Log;
 
 import com.android.internal.config.sysui.SystemUiDeviceConfigFlags;
+import com.android.internal.util.cherish.CherishUtils;
 import com.android.systemui.dagger.SysUISingleton;
 import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.settings.UserTracker;
@@ -279,7 +280,7 @@ public class QRCodeScannerController implements
         bundle.putString("caller_package", GSA_PACKAGE);
         bundle.putLong("start_activity_time_nanos", SystemClock.elapsedRealtimeNanos());
         intent.setComponent(new ComponentName(GSA_PACKAGE, LENS_ACTIVITY))
-                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK)
                 .setPackage(GSA_PACKAGE)
                 .setData(Uri.parse("google://lens"))
                 .putExtra("lens_activity_params", bundle);
@@ -301,18 +302,19 @@ public class QRCodeScannerController implements
         String prevQrCodeScannerActivity = mQRCodeScannerActivity;
         ComponentName componentName = null;
         Intent intent = new Intent();
-        if (qrCodeScannerActivity != null) {
+        if (qrCodeScannerActivity != null && !qrCodeScannerActivity.isEmpty()) {
             componentName = ComponentName.unflattenFromString(qrCodeScannerActivity);
             intent.setComponent(componentName);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
         }
 
         Intent lensIntent = getLensIntent();
-        if (isActivityAvailable(intent)) {
+        if (intent != null && isActivityAvailable(intent)) {
             mQRCodeScannerActivity = qrCodeScannerActivity;
             mComponentName = componentName;
             mIntent = intent;
-        } else if (isActivityCallable(lensIntent)) {
+        } else if (CherishUtils. isPackageInstalled(mContext, GSA_PACKAGE, false) &&
+                lensIntent != null && isActivityCallable(lensIntent)) {
             mQRCodeScannerActivity = LENS_ACTIVITY;
             mComponentName = lensIntent.getComponent();
             mIntent = lensIntent;